0a0f5ea5c30b1f1f992080deef8b230520ca9741
Kaede <37759450+Kaiddd@users.noreply.github.com>
2023-04-23 22:01:12 -0400

    2.0.4 bump
    Signed-off-by: Kaede <37759450+Kaiddd@users.noreply.github.com>

--- RCO2/RCO2.cpp
+++ RCO2/RCO2.cpp
@@ -265,7 +265,7 @@ int main(int argc, char** argv) {
         return 1;
     }
 
-    const string rcoVersionConstant = "2.0.3";
+    const string rcoVersionConstant = "2.0.4";
 
     std::ofstream rcoVerFile;
     rcoVerFile.open(rootDir + "\\programversion.rco");
